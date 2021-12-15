e1fd65e6f449098d01b5a986d24b4edfc577715c
Mahmud Chamtooni <38923023+MahmudSuleman@users.noreply.github.com>
2021-12-15 10:54:17 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+app.use(bodyParser.json());
+app.use(cors());
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI, () => {
+    console.log("connected to database");
+});
+
+const todoSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+    isCompleted: Boolean,
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+app.get("/todo", async (req, res) => {
+    // res.send("hello world");
+    const todos = await Todo.find();
+    res.send(todos);
+});
+
+app.post("/todo", async (req, res) => {
+    try {
+        const {title, body, isCompleted} = req.body;
+        const todo = new Todo();
+        todo.title = title;
+        todo.body = body;
+        todo.isCompleted = isCompleted;
+
+        const t = await todo.save();
+
+        res.send(t);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("running on port " + process.env.PORT);
+});
