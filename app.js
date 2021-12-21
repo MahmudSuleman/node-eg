f4c2de7ee98aab8d257e236c3035b28ac08b6021
Mahmud Chamtooni <38923023+MahmudSuleman@users.noreply.github.com>
2021-12-21 10:00:32 +0000

    updates

--- app.js
+++ app.js
@@ -1,49 +1,53 @@
 const express = require("express");
-const app = express();
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
-const dotenv = require("dotenv");
+
 dotenv.config();
-const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI, () => {
+mongoose.connect(
+  "mongodb+srv://mahmud:e!6pYE2f7ZdUixy@node-demo.e6bsr.mongodb.net/todos?retryWrites=true&w=majority",
+  () => {
     console.log("connected to database");
-});
-
+  }
+);
 
 const todoSchema = new mongoose.Schema({
-    title: String,
-    body: String,
-    isCompleted: Boolean,
+  title: String,
+  body: String,
+  isCompleted: Boolean,
 });
 
-const Todo = mongoose.model("Todo", todoSchema);
+const Todo = mongoose.model("todo", todoSchema);
 
 app.get("/todo", async (req, res) => {
-    // res.send("hello world");
-    const todos = await Todo.find();
-    res.send(todos);
+  // res.send("hello world");
+  const todos = await Todo.find();
+  res.send(todos);
 });
 
 app.post("/todo", async (req, res) => {
-    try {
-        const {title, body, isCompleted} = req.body;
-        const todo = new Todo();
-        todo.title = title;
-        todo.body = body;
-        todo.isCompleted = isCompleted;
+  try {
+    const { title, body, isCompleted } = req.body;
+    const todo = await Todo.create({
+      title: title,
+      body: body,
+      isCompleted: isCompleted,
+    });
+    s;
 
-        const t = await todo.save();
-
-        res.send(t);
-    } catch (err) {
-        res.send(err);
-    }
+    res.send(todo);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 app.listen(process.env.PORT, () => {
-    console.log("running on port " + process.env.PORT);
+  console.log("running on port " + process.env.PORT);
 });
