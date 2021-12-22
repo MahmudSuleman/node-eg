5aba31265a04a5a71331a87b8a20f3916f369d28
Mahmud Chamtooni <38923023+MahmudSuleman@users.noreply.github.com>
2021-12-22 14:35:15 +0000

    add extra

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 app.use(cors());
 
@@ -41,6 +41,44 @@ app.post("/todo", async (req, res) => {
     res.send(todo);
   } catch (err) {
     res.send(err);
+  }
+});
+
+// find one todo
+app.get("/todo/:id", async (req, res) => {
+  const todo = await Todo.find({
+    _id: req.params.id,
+  });
+
+  res.send(todo);
+});
+
+// update a todo
+app.put("/todo/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      body: req.body.body,
+    });
+
+    res.send(todo);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// delete a todo
+app.delete("/todo/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (todo) {
+      await todo.delete();
+      res.send("todo deleted");
+    } else {
+      res.status(404).send("todo not found, can't delete");
+    }
+  } catch (err) {
+    console.log(err);
   }
 });
 
