bbdf82828a1a4a64d0f154a9a68d2d4164dd7f0b
Mahmud Chamtooni <38923023+MahmudSuleman@users.noreply.github.com>
2022-01-04 11:14:54 +0000

    form url encoded added

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const cors = require("cors");
 
 const app = express();
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 app.use(cors());
 
 dotenv.config();
